db45a4b2d02e097f16b91949ea9c8a5f6fdad2e6
Patrícia Corlaite de Souza <83217021+PatriciaCorlaite@users.noreply.github.com>
2022-05-19 09:15:45 -0300

    Update HoHoHo.cs

--- HoHoHo.cs
+++ HoHoHo.cs
@@ -13,15 +13,19 @@
 
 using System;
 
-namespace DIO {
-  class Program {
-    static void Main(string[] args) {
-      int N = int.Parse(Console.ReadLine());
+namespace _52._36_URI_1759
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
 
-      for (int i = 0; i < N-1; i++) {
-        Console.Write("Ho ");
-      }
-      Console.WriteLine("Ho!");
+            for (int i = 0; i < N - 1; i++)
+            {
+                Console.Write("Ho ");
+            }
+            Console.WriteLine("Ho!");
+        }
     }
-  }
 }
